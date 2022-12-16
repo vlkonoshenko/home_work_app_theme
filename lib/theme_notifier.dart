66065cf755b7311a3b3fe7d76d24ecbc85ffaa90
Konoshenko Vlad <konoshenko.vl@gmail.com>
2022-12-16 16:33:04 +0300

    chore: changed theme params

--- lib/theme_notifier.dart
+++ lib/theme_notifier.dart
@@ -2,12 +2,34 @@ import 'package:flutter/material.dart';
 
 class ThemeNotifier extends ChangeNotifier {
   var _themeData = ThemeData.light();
-  var _isDark = false;
+  var _isDark = true;
 
   ThemeData get theme => _themeData;
   changeTheme() {
     _isDark = !_isDark;
-    _themeData = _isDark ? ThemeData.dark() : ThemeData.light();
+    _themeData = _isDark
+        ? ThemeData.dark().copyWith(
+            canvasColor: Colors.black54,
+            textTheme: ThemeData.light().textTheme,
+            backgroundColor: Colors.white,
+            scaffoldBackgroundColor: Colors.white,
+            cardColor: Colors.deepOrange[100],
+            cardTheme: CardTheme(),
+            unselectedWidgetColor: Colors.black54,
+            appBarTheme: AppBarTheme(
+              titleTextStyle: TextStyle(color: Colors.deepOrange[100]),
+            ),
+            bottomNavigationBarTheme: BottomNavigationBarThemeData(
+              selectedLabelStyle: TextStyle(color: Colors.deepOrange[100]),
+              selectedIconTheme: IconThemeData(
+                color: Colors.deepOrange[100],
+              ),
+              unselectedLabelStyle: TextStyle(color: Colors.black54),
+              unselectedIconTheme: IconThemeData(
+                color: Colors.black54,
+              ),
+            ))
+        : ThemeData.light();
     notifyListeners();
   }
 }
